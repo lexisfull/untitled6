6f28bbcc5bf0636336b7a9617a150b55974460fe
Aleksey <alexeypanov29@gmail.com>
2024-05-23 21:34:11 +0300

    finish

--- src/CollectionEx.java
+++ src/CollectionEx.java
@@ -1,2 +1,56 @@
-public class CollectionEx {
+import java.util.Iterator;
+
+
+public class CollectionEx<T> implements Iterable<T> {
+
+    private static final int DEFAULT_SIZE = 10;
+    int size = 0;
+
+    Object[] array;
+
+    public CollectionEx() {
+        array = new Object[DEFAULT_SIZE];
+    }
+
+    public void add(T t){
+        if(size == array.length){
+            Object[] newArray = new Object[array.length + DEFAULT_SIZE];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+        }
+        array[size++] = t;
+    }
+
+    public void delete(int index){
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T t : this) {
+            sb.append(t + " ");
+        }
+        return "CollectionEx {" +
+                "size= " + size +
+                "array= " + "[" + sb + "]" +
+                '}';
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                return array[index] == null ? null : (T) array[index++];
+            }
+        };
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CollectionEx<String> collection = new CollectionEx<>();
+        collection.add("A");
+        collection.add("B");
+        collection.add("C");
+        collection.add("D");
+        collection.add("E");
+        collection.add("F");
+        System.out.println(collection);
+        System.out.println("-------------");
+        System.out.println();
+        collection.delete(3);
+        System.out.println(collection);
     }
 }
